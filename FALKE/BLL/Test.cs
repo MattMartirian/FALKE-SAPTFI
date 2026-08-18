0e7e28b32b2bcbedf0165bb07e0bbc7ca92548b6
Matias Ezequiel Martirian <matiasezequielmartirian@protonmail.com>
2026-08-18 18:54:47 -0300

    Testeo de BD

---
+++ FALKE/BLL/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ORM;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Test
+    {
+        public void testear()
+        {
+            TestORM orm = new TestORM();
+            orm.testear();
+        }
+    }
+}
---
+++ FALKE/ORM/TestORM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace ORM
+{
+    public class TestORM
+    {
+        public void testear()
+        {
+            GestorBaseDeDatos.Instancia.EjecutarQuery(
+                "INSERT INTO [FalkeDB].[dbo].[BitacoraTable] " +
+                "([id_usuario], [modulo_bitacora], [descripcion_bitacora], [criticidad_bitacora], [fecha_hora_bitacora]) " +
+                "VALUES (1, 'Testeo', 'Registro de prueba desde testear()', 1, GETDATE())"
+            );
+        }
+    }
+}
